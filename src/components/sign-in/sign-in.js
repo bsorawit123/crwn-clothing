d2e5f091d5bb13852976ec34a838d2024bba9d90
bsorawit123 <bsorawit123@gmail.com>
2021-04-30 21:52:49 +0700

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -4,7 +4,7 @@ import "./sign-in.scss";
 import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends Component {
   constructor(props) {
@@ -16,10 +16,17 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   handleChange = (e) => {
